dc841ab0c51d16e6f377fe5d893cf4c0921fc999
Rahmatfadli09 <rahmatfadli@student.uir.ac.id>
2023-05-20 15:43:02 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="black">{{ msg }}</h1>
+    <h3>
+      <a href="https://instagram.com/rmt.fadli?igshid=ZGUzMzM3NWJiOQ==" target="_blank" rel="noopener"><img src="@/assets/ig.jpeg" alt=""></a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener"><img src="@/assets/github.png" alt=""></a>
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+img {
+  width: 8%;
+  border: 2px solid black;
+  border-radius: 50%;
+  
+}
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+  color: black;
+}
+
+h3 a {
+  font-size: 1.2rem;
+  color: black;
+  
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+  <h1> DATA DIRI</h1>
+    <p align="right">Nama Lengkap : Rahmat Fadli</p><br>
+    <p align="right">NPM : 213510594</P><br>
+    <p align="right">TTL : Bukittinggi, 9 NOVEMBER 2002</p>
+
+</main>
+</template>
+<style>
+
+h1 {
+    text-align: center;
+    font-size: 25px;
+    font-weight: bold;
+    margin: 20px 0;
+    text-transform: uppercase;
+    color: black;
+}
+
+p {
+    text-align: center;
+    font-size: 18px;
+    margin: auto;
+    color: black;
+}
+
+
+</style>
